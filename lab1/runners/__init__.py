caaf7896af5e687d00a135edc856c133e9bb8c25
Vaniog <forvantar@gmail.com>
2024-03-24 17:16:30 +0300

    some changes fuck me

--- lab1/runners/__init__.py
+++ lab1/runners/__init__.py
@@ -1,5 +1,5 @@
 from .coordinate_descend_runner import *
 from .coordinate_gradient_runner import *
 from .gradient_descent_runner import *
-from .methodNidelMid import *
+from .nelder_mead_runner import *
 from .coordinate_gradient_runner import *
