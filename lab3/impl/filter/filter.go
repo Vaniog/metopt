650084e2823a2ba55b34c4643ddd05f8c6d0449c
matvi <matvik.gurov@gmail.com>
2024-05-15 02:10:35 +0300

    filter

--- lab3/impl/filter/filter.go
+++ lab3/impl/filter/filter.go
@@ -6,3 +6,15 @@ func Map[T any, U any](ts []T, mapFunc func(T) U) (us []U) {
 	}
 	return
 }
+
+func MapWithError[T any, U any](ts []T, mapFunc func(T) (U, error)) (us []U, err error) {
+	var res U
+	for i := range ts {
+		res, err = mapFunc(ts[i])
+		if err != nil {
+			return
+		}
+		us = append(us, res)
+	}
+	return
+}
